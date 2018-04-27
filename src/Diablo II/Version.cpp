929204c41952d554dffccd8cea6994e59820e48d
IAmTrial <IAmRealTrial@gmail.com>
2018-04-26 22:36:17 -0700

    Add version detection from file version string
    Game version is detected using the game executable's file version using
    an unordered map for instant searching.
    
    However, this is a solution that only works with LoD players. A new
    solution will be needed for classic clients. A TODO notes this issue.

--- src/Diablo II/Version.cpp
+++ src/Diablo II/Version.cpp
@@ -25,6 +25,7 @@
 #include <string>
 #include <string_view>
 #include <vector>
+#include <unordered_map>
 
 #include "../Common/Common.h"
 
@@ -33,13 +34,18 @@ namespace {
 
 constexpr const char *kGameExecutable = "Game.exe";
 std::string ExtractFileVersionString(std::string_view file_name);
+enum GameVersion GetGameVersionByFileVersion(std::string_view file_version);
 
 } // namespace
 
 enum GameVersion GetGameVersion() {
+    // TODO(Mir Drualga): Figure out how to get version number without using
+    // file version. This is important for classic D2 client compatibility!
     static std::string game_version_string = ExtractFileVersionString(
             kGameExecutable);
-    static enum GameVersion game_version = GameVersion::kInvalid;
+    static enum GameVersion game_version = GetGameVersionByFileVersion(
+            game_version_string);
+
     return game_version;
 }
 
@@ -47,7 +53,7 @@ namespace {
 
 std::string ExtractFileVersionString(std::string_view file_name) {
     // All the code for this function originated from StackOverflow user
-    // crashmstr.
+    // crashmstr. Some parts were refactored for clarity.
 
     DWORD version_handle;
 
@@ -91,6 +97,34 @@ std::string ExtractFileVersionString(std::string_view file_name) {
 
     return stringStream.str();
 }
+
+enum GameVersion GetGameVersionByFileVersion(std::string_view version_string) {
+        static const std::unordered_map<std::string_view, enum GameVersion>
+                game_versions_by_file_version = {
+            { "1.0.7.0", GameVersion::kLod1_07 },
+            { "1.0.8.28", GameVersion::kLod1_08 },
+            { "1.0.9.19", GameVersion::kLod1_09 },
+            { "1.0.9.20", GameVersion::kLod1_09B },
+            { "1.0.9.21", GameVersion::kLod1_09C },
+            { "1.0.9.22", GameVersion::kLod1_09D },
+            { "1.0.10.39", GameVersion::kLod1_10 },
+            { "1.0.11.45", GameVersion::kLod1_11 },
+            { "1.0.11.46", GameVersion::kLod1_11B },
+            { "1.0.12.49", GameVersion::kLod1_12 },
+            { "1.0.13.60", GameVersion::kLod1_13C },
+            { "1.0.13.64", GameVersion::kLod1_13D },
+            { "1.14.0.64", GameVersion::kLod1_14A },
+            { "1.14.1.68", GameVersion::kLod1_14B },
+            { "1.14.2.70", GameVersion::kLod1_14C },
+            { "1.14.3.71", GameVersion::kLod1_14D }
+        };
+
+        auto found_version_pair = game_versions_by_file_version.find(
+                version_string);
+        return (found_version_pair != game_versions_by_file_version.cend())
+                ? found_version_pair->second
+                : GameVersion::kInvalid;
+    }
 
 } // namespace
 
