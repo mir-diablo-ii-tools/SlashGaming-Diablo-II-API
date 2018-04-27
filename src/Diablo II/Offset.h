405a38fa949e3b97595e30d6e424e109e2ffcaa7
IAmTrial <IAmRealTrial@gmail.com>
2018-04-27 11:21:24 -0700

    Define the Offset class

--- src/Diablo II/Offset.h
+++ src/Diablo II/Offset.h
@@ -17,7 +17,80 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 #ifndef SGD2MAPI_DIABLOII_OFFSET_H_
 #define SGD2MAPI_DIABLOII_OFFSET_H_
+
+#include <windows.h>
+#include <cstdint>
+#include <string>
+#include <string_view>
+#include <unordered_map>
+
+#include "GameLibrary.h"
+#include "Version.h"
+
+#ifdef SGD2MAPI_DLLEXPORT
+#define DLLEXPORT __declspec(dllexport)
+#else
+#ifdef SGD2MAPI_DLLIMPORT
+#define DLLEXPORT __declspec(dllimport)
+#else
+#define DLLEXPORT
+#endif
+#endif
+
+namespace slashgaming::diabloii {
+
+class DLLEXPORT Offset {
+public:
+    /**
+     * Creates an instance of Offset by specifying the game library associated
+     * with the specified map of offset values. The map contains a mapping of
+     * the game versions to the offset values.
+     */
+    Offset(gamelibrary::GameLibraries game_library,
+            const std::unordered_map<version::GameVersion,
+                    uint64_t>& offsets_by_game_versions);
+
+    /**
+     * Creates an instance of Offset by specifying the file name of the library
+     * associated with the specified map of offset values. The map contains a
+     * mapping of the game versions to the offset values.
+     */
+    Offset(std::string_view library_name,
+            const std::unordered_map<version::GameVersion,
+                    uint64_t>& offsets_by_game_versions);
+
+    Offset& operator=(const Offset& offset) = default;
+
+    explicit Offset(Offset&& offset) = default;
+    Offset& operator=(Offset&& rhs) = default;
+
+    /**
+     * Calculates and returns the target address of the offset by adding the
+     * offset associated with the running game version to the base address of
+     * the offset's library.
+     */
+    HMODULE CalculateAddress();
+
+    /**
+     * Returns the game offset associated with the running game version.
+     */
+    HMODULE GetRunningGameOffset();
+
+    /**
+     * Returns the file name of this offset's library.
+     */
+    std::string get_library_file_name();
+private:
+    std::string library_file_name_;
+    std::unordered_map<version::GameVersion,
+           long long int>& offsets_by_game_versions_;
+};
+
+} // slashgaming::diabloii
+
+#undef DLLEXPORT
 
 #endif // SGD2MAPI_DIABLOII_OFFSET_H_
