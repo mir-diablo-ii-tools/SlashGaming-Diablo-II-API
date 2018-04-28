669759171d984b828bb7af5a546ffef326249c20
IAmTrial <IAmRealTrial@gmail.com>
2018-04-27 20:54:13 -0700

    Implement InterceptionPatch
    InterceptionPatch replaces game bytes with the intention of running a
    function.
    
    The implementation uses vector in order to allow for a dynamically sized
    buffer to exist until the end of the function's scope. This vector is
    used as a buffer to store all the changes that will be made to the
    game's bytes.

--- src/Diablo II/Patch/InterceptionPatch.cpp
+++ src/Diablo II/Patch/InterceptionPatch.cpp
@@ -19,3 +19,46 @@
  */
 
 #include "InterceptionPatch.h"
+
+#include <cstdint>
+#include <cstring>
+#include <vector>
+
+#include "BasePatch.h"
+
+namespace slashgaming::diabloii::patch {
+
+void InterceptionPatch::Apply() {
+    if (is_patch_applied()) {
+        return;
+    }
+
+    // Fill the container with no-op.
+    std::vector<uint8_t> replace_buffer(get_patch_size(),
+            static_cast<uint8_t>(OpCodes::kNop));
+
+    // Replace the first byte with the opcode.
+    replace_buffer.at(0) = static_cast<uint8_t>(get_opcode());
+
+    // Replace the next bytes with the function pointer.
+    uintptr_t func_ptr = get_func_ptr();
+    std::memmove(&replace_buffer.at(1), &func_ptr, sizeof(func_ptr));
+
+    // Replace the game bytes with the bytes in the buffer.
+    uintptr_t address = get_offset().CalculateAddress();
+    std::memmove(reinterpret_cast<void*>(address), replace_buffer.data(),
+            sizeof(func_ptr));
+
+    // Set applied to true.
+    BasePatch::Apply();
+}
+
+OpCodes InterceptionPatch::get_opcode() const {
+    return opcode_;
+}
+
+uintptr_t InterceptionPatch::get_func_ptr() const {
+    return func_ptr_;
+}
+
+} // namespace slashgaming::diabloii::patch
