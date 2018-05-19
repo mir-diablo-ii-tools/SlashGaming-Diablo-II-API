8851d557a4d92a6fa964d552d59d7e10880c5187
IAmTrial <IAmRealTrial@gmail.com>
2018-05-19 11:04:10 -0700

    Create D2GFXFunc and add D2GFX_DrawLine

---
+++ src/Diablo II/Pointer/D2GFXFunc.cpp
@@ -0,0 +1,70 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "D2GFXFunc.h"
+
+#include <cstdint>
+#include <functional>
+#include <unordered_map>
+
+#include "../GameLibrary.h"
+#include "../Pointer.h"
+#include "../Version.h"
+
+namespace slashgaming::diabloii::func {
+
+namespace {
+
+void __stdcall D2GFX_DrawLine_1_13C(int x1, int y1, int x2, int y2,
+        unsigned int color, unsigned int unknown);
+
+extern "C" {
+}
+
+} // namespace
+
+void D2GFX_DrawLine(int x1, int y1, int x2, int y2, unsigned int color,
+        unsigned int unknown) {
+    switch (GetGameVersion()) {
+        case GameVersion::k1_13C:
+        case GameVersion::k1_13D: {
+            D2GFX_DrawLine_1_13C(x1, y1, x2, y2, color, unknown);
+            break;
+        }
+    }
+}
+
+namespace {
+
+void __stdcall D2GFX_DrawLine_1_13C(int x1, int y1, int x2, int y2,
+        unsigned int color, unsigned int unknown) {
+    static const auto func =
+            reinterpret_cast<decltype(D2GFX_DrawLine_1_13C)*>(
+                Pointer(GameLibraries::kD2Client, {
+                    { GameVersion::k1_13C, { PointerType::kOrdinal, 10010 } },
+                    { GameVersion::k1_13D, { PointerType::kOrdinal, 10013 } }
+                }).get_address());
+
+    func(x1, y1, x2, y2, color, unknown);
+}
+
+} // namespace
+
+} // namespace slashgaming::diabloii::func
---
+++ src/Diablo II/Pointer/D2GFXFunc.h
@@ -0,0 +1,45 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SGD2MAPI_DIABLOII_POINTER_D2GFXFUNC_H_
+#define SGD2MAPI_DIABLOII_POINTER_D2GFXFUNC_H_
+
+#ifdef SGD2MAPI_DLLEXPORT
+#define DLLEXPORT __declspec(dllexport)
+#else
+#ifdef SGD2MAPI_DLLIMPORT
+#define DLLEXPORT __declspec(dllimport)
+#else
+#define DLLEXPORT
+#endif
+#endif
+
+namespace slashgaming::diabloii::func {
+
+extern "C" {
+    DLLEXPORT void D2GFX_DrawLine(int x1, int y1, int x2, int y2,
+            unsigned int color, unsigned int unknown);
+}
+
+} // namespace slashgaming::diabloii::func
+
+#undef DLLEXPORT
+
+#endif // SGD2MAPI_DIABLOII_POINTER_D2GFXFUNC_H_
