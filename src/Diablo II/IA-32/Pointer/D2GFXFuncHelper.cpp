59e6fa0c7d2c51d831093ca287455887d760d3f4
IAmTrial <IAmRealTrial@gmail.com>
2018-05-19 16:26:26 -0700

    Create D2Client and D2GFX function helper headers

---
+++ src/Diablo II/IA-32/Pointer/D2GFXFuncHelper.cpp
@@ -0,0 +1,34 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__i386__) || defined(_M_IX86) || defined(_X86_)
+
+#define SGD2MAPI_DIABLOII_POINTER_D2GFXFUNC_H_INCLUDE_
+#include "D2GFXFuncHelper.h"
+#undef SGD2MAPI_DIABLOII_POINTER_D2GFXFUNC_H_INCLUDE_
+
+#include "../../GameLibrary.h"
+#include "../../Pointer.h"
+#include "../../Version.h"
+
+namespace slashgaming::diabloii::func {
+} // slashgaming::diabloii::func
+
+#endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
