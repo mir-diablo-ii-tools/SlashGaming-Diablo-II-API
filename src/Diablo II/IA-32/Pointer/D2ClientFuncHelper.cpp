c8da623fc3b0d85728d919ebcaf2819f6b47a7a5
IAmTrial <IAmRealTrial@gmail.com>
2018-05-19 17:27:28 -0700

    Move function definition to its intended file

--- src/Diablo II/IA-32/Pointer/D2ClientFuncHelper.cpp
+++ src/Diablo II/IA-32/Pointer/D2ClientFuncHelper.cpp
@@ -32,18 +32,6 @@
 
 namespace slashgaming::diabloii::func {
 
-void __stdcall D2GFX_DrawLine_1_13C(int x1, int y1, int x2, int y2,
-        unsigned int color, unsigned int unknown) {
-    static const auto func =
-            reinterpret_cast<decltype(D2GFX_DrawLine_1_13C)*>(
-                Pointer(GameLibraries::kD2Client, {
-                    { GameVersion::k1_13C, { PointerType::kOrdinal, 10010 } },
-                    { GameVersion::k1_13D, { PointerType::kOrdinal, 10013 } }
-                }).get_address());
-
-    func(x1, y1, x2, y2, color, unknown);
-}
-
 } // namespace slashgaming::diabloii::func
 
 #endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
