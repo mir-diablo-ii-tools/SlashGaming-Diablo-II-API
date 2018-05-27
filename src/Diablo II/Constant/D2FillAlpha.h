5616b47986aaeaf26075940cad69bc65af4cd786
IAmTrial <IAmRealTrial@gmail.com>
2018-05-27 11:40:18 -0700

    Implement D2FillAlpha and its Ex class

---
+++ src/Diablo II/Constant/D2FillAlpha.h
@@ -0,0 +1,91 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SGD2MAPI_DIABLOII_CONSTANT_D2FILLALPHA_H_
+#define SGD2MAPI_DIABLOII_CONSTANT_D2FILLALPHA_H_
+
+#include "D2BaseConstant.h"
+
+#include <type_traits>
+
+namespace slashgaming::diabloii::constant {
+
+enum class D2FillAlphas {
+    k0PercentOpaque,
+    k50PercentOpaque,
+    k75PercentOpaque,
+    k90PercentOpaque,
+    k100PercentOpaque,
+    k90PercentBlack,
+    k100PercentBlack,
+};
+
+/**
+ * A wrapper class to represent the D2FillAlpha constant in a version and
+ * architecture agnostic way.
+ */
+class ExD2FillAlpha {
+public:
+    /**
+     * Creates an instance of ExD2FillAlpha, resolving the value of the constant
+     * to an integer based on the running game version and architecture.
+     */
+    explicit ExD2FillAlpha(enum D2FillAlphas fill_alpha);
+
+    explicit ExD2FillAlpha(const ExD2FillAlpha& base_constant) = default;
+    explicit ExD2FillAlpha(ExD2FillAlpha&& base_constant) = default;
+
+    ExD2FillAlpha& operator=(const ExD2FillAlpha& rhs) = default;
+    ExD2FillAlpha& operator=(ExD2FillAlpha&& rhs) = default;
+
+    /**
+     * Returns the resolved value that corresponds to this instance of the
+     * constant.
+     */
+    explicit operator int() const;
+
+    /**
+     * Returns the value of the constant that corresponds to this instance of
+     * the constant.
+     */
+    explicit operator enum D2FillAlphas() const;
+
+    /**
+     * Returns the resolved value that corresponds to this instance of the
+     * constant.
+     */
+    int get_value() const;
+
+    /**
+     * Returns the value of the constant that corresponds to this instance of
+     * the constant.
+     */
+    enum D2FillAlphas get_fill_alpha() const;
+
+private:
+    enum D2FillAlphas fill_alpha_;
+    int value_;
+
+    static int ResolveValue(enum D2FillAlphas fill_alpha);
+};
+
+} // namespace slashgaming::diabloii::constant
+
+#endif // SGD2MAPI_DIABLOII_IA_32_CONSTANT_EXD2FILLALPHAS_H_
---
+++ src/Diablo II/IA-32/Constant/D2FillAlpha.cpp
@@ -0,0 +1,91 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__i386__) || defined(_M_IX86) || defined(_X86_)
+
+#include "../../Constant/D2FillAlpha.h"
+
+namespace slashgaming::diabloii::constant {
+namespace {
+
+constexpr int ResolveValue_1_00(enum D2FillAlphas fill_alpha) {
+    switch (fill_alpha) {
+        case D2FillAlphas::k50PercentOpaque: {
+            return 0;
+        }
+
+        case D2FillAlphas::k75PercentOpaque: {
+            return 1;
+        }
+
+        case D2FillAlphas::k90PercentOpaque: {
+            return 2;
+        }
+
+        case D2FillAlphas::k0PercentOpaque: {
+            return 3;
+        }
+
+        case D2FillAlphas::k100PercentBlack: {
+            return 4;
+        }
+
+        case D2FillAlphas::k100PercentOpaque: {
+            return 5;
+        }
+
+        case D2FillAlphas::k90PercentBlack: {
+            return 6;
+        }
+    }
+
+    // Should never happen.
+    return -1;
+}
+
+} // namespace
+
+ExD2FillAlpha::ExD2FillAlpha(enum D2FillAlphas fill_alpha) :
+        fill_alpha_(fill_alpha), value_(ResolveValue(fill_alpha)) {
+}
+
+int ExD2FillAlpha::ResolveValue(enum D2FillAlphas fill_alpha) {
+    return ResolveValue_1_00(fill_alpha);
+}
+
+ExD2FillAlpha::operator int() const {
+    return get_value();
+}
+
+ExD2FillAlpha::operator enum D2FillAlphas() const {
+    return get_fill_alpha();
+}
+
+int ExD2FillAlpha::get_value() const {
+    return value_;
+}
+
+enum D2FillAlphas ExD2FillAlpha::get_fill_alpha() const {
+    return fill_alpha_;
+}
+
+} // namespace slashgaming::diabloii::constant
+
+#endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
