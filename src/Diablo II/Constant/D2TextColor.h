3ea63f65633c53b583dc2ba5bb041506c722203d
IAmTrial <IAmRealTrial@gmail.com>
2018-05-27 15:39:01 -0700

    Define D2TextColors constant and its Ex class

---
+++ src/Diablo II/Constant/D2TextColor.h
@@ -0,0 +1,100 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef SGD2MAPI_DIABLOII_CONSTANT_D2TEXTCOLOR_H_
+#define SGD2MAPI_DIABLOII_CONSTANT_D2TEXTCOLOR_H_
+
+#ifdef SGD2MAPI_DLLEXPORT
+#define DLLEXPORT __declspec(dllexport)
+#elif defined(SGD2MAPI_DLLIMPORT)
+#define DLLEXPORT __declspec(dllimport)
+#else
+#define DLLEXPORT
+#endif
+
+namespace slashgaming::diabloii::constant {
+
+/**
+ * The different colors used to display text in Diablo II.
+ */
+enum class D2TextColors {
+    kBlack,
+    kBlue,
+    kCorrupted,
+    kVomit = kCorrupted,
+    kDarkGold,
+    kGold,
+    kDarkGreen,
+    kDarkerGreen,
+    kLightGreen,
+    kDarkGrey,
+    kLightGrey,
+    kMetallic,
+    kOrange,
+    kPurple,
+    kLightRed,
+    kDarkRed,
+    kBrightWhite,
+    kWhite,
+    kYellow,
+};
+
+/**
+ * A wrapper class to represent the D2TextColor constant in a version and
+ * architecture agnostic way.
+ */
+class DLLEXPORT ExD2TextColor {
+public:
+    /**
+     * Creates an instance of ExD2TextColor, resolving the value of the constant
+     * to an integer based on the running game version and architecture.
+     */
+    explicit ExD2TextColor(enum D2TextColors fill_alpha);
+
+    explicit ExD2TextColor(const ExD2TextColor& base_constant) = default;
+    explicit ExD2TextColor(ExD2TextColor&& base_constant) = default;
+
+    ExD2TextColor& operator=(const ExD2TextColor& rhs) = default;
+    ExD2TextColor& operator=(ExD2TextColor&& rhs) = default;
+
+    /**
+     * Returns the resolved value that corresponds to this instance of the
+     * constant.
+     */
+    int value() const;
+
+    /**
+     * Returns the value of the constant that corresponds to this instance of
+     * the constant. The underlying value of returned does not necessarily
+     * correspond to that used in the game. Use value() for that purpose.
+     */
+    enum D2TextColors text_color() const;
+
+private:
+    enum D2TextColors text_color_;
+    int value_;
+
+    static int ResolveValue(enum D2TextColors text_color);
+};
+
+} // namespace slashgaming::diabloii::constant
+
+#undef DLLEXPORT
+#endif // SGD2MAPI_DIABLOII_CONSTANT_D2TEXTCOLOR_H_
---
+++ src/Diablo II/IA-32/Constant/D2TextColor.cpp
@@ -0,0 +1,129 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__i386__) || defined(_M_IX86) || defined(_X86_)
+
+#include "../../Constant/D2TextColor.h"
+
+namespace slashgaming::diabloii::constant {
+namespace {
+
+constexpr int ResolveValue_1_00(enum D2TextColors text_color) {
+    switch (text_color) {
+        case D2TextColors::kBlue: {
+            return 3;
+        }
+
+        case D2TextColors::kBlack: {
+            // Also has the value 19.
+            return 6;
+        }
+
+        case D2TextColors::kCorrupted: {
+            return 16;
+        }
+
+        case D2TextColors::kDarkGold: {
+            return 4;
+        }
+
+        case D2TextColors::kGold: {
+            return 7;
+        }
+
+        case D2TextColors::kDarkGreen: {
+            return 12;
+        }
+
+        case D2TextColors::kDarkerGreen: {
+            return 10;
+        }
+
+        case D2TextColors::kLightGreen: {
+            return 2;
+        }
+
+        case D2TextColors::kDarkGrey: {
+            return 5;
+        }
+
+        case D2TextColors::kLightGrey: {
+            return 15;
+        }
+
+        case D2TextColors::kMetallic: {
+            return 14;
+        }
+
+        case D2TextColors::kOrange: {
+            return 8;
+        }
+
+        case D2TextColors::kPurple: {
+            return 11;
+        }
+
+        case D2TextColors::kLightRed: {
+            return 1;
+        }
+
+        case D2TextColors::kDarkRed: {
+            return 18;
+        }
+
+        case D2TextColors::kBrightWhite: {
+            return 17;
+        }
+
+        case D2TextColors::kWhite: {
+            // Also has the values 13 and 20.
+            return 0;
+        }
+
+        case D2TextColors::kYellow: {
+            return 9;
+        }
+    }
+
+    // Should never happen.
+    return -1;
+}
+
+} // namespace
+
+ExD2TextColor::ExD2TextColor(enum D2TextColors text_color) :
+        text_color_(text_color), value_(ResolveValue(text_color)) {
+}
+
+int ExD2TextColor::ResolveValue(enum D2TextColors text_color) {
+    return ResolveValue_1_00(text_color);
+}
+
+int ExD2TextColor::value() const {
+    return value_;
+}
+
+enum D2TextColors ExD2TextColor::text_color() const {
+    return text_color_;
+}
+
+} // namespace slashgaming::diabloii::constant
+
+#endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
