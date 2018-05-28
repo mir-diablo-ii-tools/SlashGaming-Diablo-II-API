84c1434733e2e8950192e0c465151423511e58ee
IAmTrial <IAmRealTrial@gmail.com>
2018-05-28 10:46:01 -0700

    Create definition and helper files for DLL funcs

---
+++ src/Diablo II/IA-32/Pointer/Func/D2CMPFuncHelper.cpp
@@ -0,0 +1,37 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__i386__) || defined(_M_IX86) || defined(_X86_)
+
+#define SGD2MAPI_DIABLOII_IA_32_POINTER_FUNC_D2CMPFUNCHELPER_H_INCLUDE_
+#include "D2CMPFuncHelper.h"
+#undef SGD2MAPI_DIABLOII_IA_32_POINTER_FUNC_D2CMPFUNCHELPER_H_INCLUDE_
+
+#include <unordered_map>
+
+#include "../../../GameLibrary.h"
+#include "../../../Pointer.h"
+#include "../../../Version.h"
+
+namespace slashgaming::diabloii::func {
+
+} // namespace slashgaming::diabloii::func
+
+#endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
---
+++ src/Diablo II/IA-32/Pointer/Func/D2GlideFuncStubs.h
@@ -0,0 +1,26 @@
+/**
+ * SlashGaming Diablo II Modding API
+ * Copyright (C) 2018  SlashGaming Community
+ *
+ * This file is part of SlashGaming Diablo II Modding API.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published
+ *  by the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#if defined(__i386__) || defined(_M_IX86) || defined(_X86_)
+#ifndef SGD2MAPI_DIABLOII_IA_32_POINTER_FUNC_D2GLIDEFUNCSTUBS_H_
+#define SGD2MAPI_DIABLOII_IA_32_POINTER_FUNC_D2GLIDEFUNCSTUBS_H_
+
+#endif // SGD2MAPI_DIABLOII_IA_32_POINTER_FUNC_D2GLIDEFUNCSTUBS_H_
+#endif // defined(__i386__) || defined(_M_IX86) || defined(_X86_)
