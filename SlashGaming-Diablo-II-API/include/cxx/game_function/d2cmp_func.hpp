849551e26d7f2dea7846bd4924f3a550dd424762
IAmTrial <IAmRealTrial@gmail.com>
2020-04-08 22:48:18 -0700

    Refactor D2CMP function include file

--- SlashGaming-Diablo-II-API/include/cxx/game_function/d2cmp_func.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/game_function/d2cmp_func.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -43,9 +43,9 @@
  *  work.
  */
 
-#ifndef SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNC_HPP_
-#define SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNC_HPP_
+#ifndef SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNCTION_HPP_
+#define SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNCTION_HPP_
 
 #include "d2cmp/d2cmp_get_cel_from_cel_context.hpp"
 
-#endif // SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNC_HPP_
+#endif // SGD2MAPI_CXX_GAME_FUNCTION_D2CMP_FUNCTION_HPP_
