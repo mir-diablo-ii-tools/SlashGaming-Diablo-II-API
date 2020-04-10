98d28258203e3e32e1c6e7f9c7f014c3a6931a38
IAmTrial <IAmRealTrial@gmail.com>
2020-04-09 23:19:34 -0700

    Rename empty function include files

--- SlashGaming-Diablo-II-API/include/cxx/game_function/d2net_function.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/game_function/d2net_function.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -43,7 +43,7 @@
  *  work.
  */
 
-#ifndef SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNC_HPP_
-#define SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNC_HPP_
+#ifndef SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNCTION_HPP_
+#define SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNCTION_HPP_
 
-#endif // SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNC_HPP_
+#endif // SGD2MAPI_CXX_GAME_FUNCTION_D2NET_FUNCTION_HPP_
