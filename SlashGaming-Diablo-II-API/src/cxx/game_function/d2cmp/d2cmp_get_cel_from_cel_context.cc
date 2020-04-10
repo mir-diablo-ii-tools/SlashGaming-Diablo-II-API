e876d2c92b74638a209011074a115bf8cc08a333
IAmTrial <IAmRealTrial@gmail.com>
2020-04-09 17:44:07 -0700

    Remove lower bound restrictions below 1.00

--- SlashGaming-Diablo-II-API/src/cxx/game_function/d2cmp/d2cmp_get_cel_from_cel_context.cc
+++ SlashGaming-Diablo-II-API/src/cxx/game_function/d2cmp/d2cmp_get_cel_from_cel_context.cc
@@ -73,8 +73,7 @@ static const mapi::GameAddress& GetGameAddress() {
 Cel* GetCelFromCelContext(CelContext* cel_context) {
   GameVersion running_game_version = GetRunningGameVersionId();
 
-  if (running_game_version >= GameVersion::k1_00
-      && running_game_version <= GameVersion::k1_10) {
+  if (running_game_version <= GameVersion::k1_10) {
     auto* actual_cel_context = reinterpret_cast<CelContext_1_00*>(
         cel_context
     );
