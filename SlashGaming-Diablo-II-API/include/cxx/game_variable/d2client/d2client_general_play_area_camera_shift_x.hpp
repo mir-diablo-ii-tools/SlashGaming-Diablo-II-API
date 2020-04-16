d62f8d3b314e121b3c945c279372ea107aaaed85
IAmTrial <IAmRealTrial@gmail.com>
2020-04-15 17:55:07 -0700

    Refactor D2Client GeneralPlayAreaCameraShiftX var

--- SlashGaming-Diablo-II-API/include/cxx/game_variable/d2client/d2client_general_play_area_camera_shift_x.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/game_variable/d2client/d2client_general_play_area_camera_shift_x.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -46,12 +46,19 @@
 #ifndef SGD2MAPI_CXX_GAME_VARIABLE_D2CLIENT_D2CLIENT_GENERAL_PLAY_AREA_CAMERA_SHIFT_X_HPP_
 #define SGD2MAPI_CXX_GAME_VARIABLE_D2CLIENT_D2CLIENT_GENERAL_PLAY_AREA_CAMERA_SHIFT_X_HPP_
 
+#include <cstdint>
+
 #include "../../../dllexport_define.inc"
 
 namespace d2::d2client {
 
 DLLEXPORT int GetGeneralPlayAreaCameraShiftX();
-DLLEXPORT void SetGeneralPlayAreaCameraShiftX(int value);
+
+DLLEXPORT std::int32_t GetGeneralPlayAreaCameraShiftX_1_00();
+
+DLLEXPORT void SetGeneralPlayAreaCameraShiftX(int camera_shift_x);
+
+DLLEXPORT void SetGeneralPlayAreaCameraShiftX_1_00(std::int32_t camera_shift_x);
 
 } // namespace d2::d2client
 
