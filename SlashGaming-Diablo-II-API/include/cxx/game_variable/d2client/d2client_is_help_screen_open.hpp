2894ac95036a630d75c9a3422f81d3964fc22e71
IAmTrial <IAmRealTrial@gmail.com>
2020-04-15 23:28:03 -0700

    Refactor D2Client IsHelpScreenOpen variable

--- SlashGaming-Diablo-II-API/include/cxx/game_variable/d2client/d2client_is_help_screen_open.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/game_variable/d2client/d2client_is_help_screen_open.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -46,12 +46,19 @@
 #ifndef SGD2MAPI_CXX_GAME_VARIABLE_D2CLIENT_D2CLIENT_IS_HELP_SCREEN_OPEN_HPP_
 #define SGD2MAPI_CXX_GAME_VARIABLE_D2CLIENT_D2CLIENT_IS_HELP_SCREEN_OPEN_HPP_
 
+#include "../../game_bool.hpp"
+
 #include "../../../dllexport_define.inc"
 
 namespace d2::d2client {
 
 DLLEXPORT bool GetIsHelpScreenOpen();
-DLLEXPORT void SetIsHelpScreenOpen(bool value);
+
+DLLEXPORT mapi::bool32 GetIsHelpScreenOpen_1_00();
+
+DLLEXPORT void SetIsHelpScreenOpen(bool is_help_screen_open);
+
+DLLEXPORT void SetIsHelpScreenOpen_1_00(mapi::bool32 is_help_screen_open);
 
 } // namespace d2::d2client
 
