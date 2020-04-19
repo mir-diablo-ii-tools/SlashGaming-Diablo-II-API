47dc8b82e3ed5f039af000fbacdaebe1989cf2b8
IAmTrial <IAmRealTrial@gmail.com>
2020-04-19 01:12:01 -0700

    Refactor DrawOptions helper

--- SlashGaming-Diablo-II-API/include/cxx/helper/d2_draw_options.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/helper/d2_draw_options.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -68,14 +68,14 @@ enum class DrawPositionYBehavior {
 };
 
 struct DrawCelFileFrameOptions {
-  mapi::RGBA32BitColor color;
+  mapi::Rgba32BitColor color;
   DrawEffect draw_effect;
   DrawPositionXBehavior position_x_behavior;
   DrawPositionYBehavior position_y_behavior;
 };
 
 struct DrawAllCelFileFramesOptions {
-  mapi::RGBA32BitColor color;
+  mapi::Rgba32BitColor color;
   DrawEffect draw_effect;
   DrawPositionXBehavior position_x_behavior;
   DrawPositionYBehavior position_y_behavior;
