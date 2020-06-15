f260c493061c4fea21b3d99866495c9820a5a991
IAmTrial <IAmRealTrial@gmail.com>
2020-06-14 21:03:51 -0700

    Refactor EquipmentLayout API

--- SlashGaming-Diablo-II-API/include/cxx/game_struct/d2_equipment_layout/d2_equipment_layout_api.hpp
+++ SlashGaming-Diablo-II-API/include/cxx/game_struct/d2_equipment_layout/d2_equipment_layout_api.hpp
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -60,26 +60,22 @@
 
 namespace d2 {
 
-class DLLEXPORT EquipmentLayout_API {
+class DLLEXPORT EquipmentLayout_Api {
  public:
-  EquipmentLayout_API() = delete;
-  EquipmentLayout_API(
+  EquipmentLayout_Api() = delete;
+  EquipmentLayout_Api(
       PositionalRectangle_View position,
-      std::int_least8_t width,
-      std::int_least8_t height
+      unsigned char width,
+      unsigned char height
   );
 
-  EquipmentLayout_API(const EquipmentLayout_API& other);
-  EquipmentLayout_API(EquipmentLayout_API&& other) noexcept;
+  EquipmentLayout_Api(const EquipmentLayout_Api& other);
+  EquipmentLayout_Api(EquipmentLayout_Api&& other) noexcept;
 
-  ~EquipmentLayout_API();
+  ~EquipmentLayout_Api();
 
-  EquipmentLayout_API& operator=(
-      const EquipmentLayout_API& other
-  );
-  EquipmentLayout_API& operator=(
-      EquipmentLayout_API&& other
-  ) noexcept;
+  EquipmentLayout_Api& operator=(const EquipmentLayout_Api& other);
+  EquipmentLayout_Api& operator=(EquipmentLayout_Api&& other) noexcept;
 
   operator EquipmentLayout_View() const noexcept;
   operator EquipmentLayout_Wrapper() noexcept;
@@ -87,21 +83,28 @@ class DLLEXPORT EquipmentLayout_API {
   EquipmentLayout* Get() noexcept;
   const EquipmentLayout* Get() const noexcept;
 
-  void Copy(EquipmentLayout_View src) noexcept;
+  void Assign(EquipmentLayout_View src) noexcept;
 
   PositionalRectangle* GetPosition() noexcept;
   const PositionalRectangle* GetPosition() const noexcept;
 
-  std::uint_least8_t GetWidth() const noexcept;
-  void SetWidth(std::uint_least8_t value) noexcept;
+  unsigned char GetWidth() const noexcept;
+  void SetWidth(unsigned char width) noexcept;
 
-  std::uint_least8_t GetHeight() const noexcept;
-  void SetHeight(std::uint_least8_t value) noexcept;
+  unsigned char GetHeight() const noexcept;
+  void SetHeight(unsigned char height) noexcept;
 
  private:
-  std::variant<
-      std::unique_ptr<EquipmentLayout_1_00>
-  > equipment_layout_;
+  using unique_ptr_1_00 = std::unique_ptr<EquipmentLayout_1_00>;
+  using ptr_variant = std::variant<unique_ptr_1_00>;
+
+  ptr_variant equipment_layout_;
+
+  static ptr_variant CreateVariant(
+      const PositionalRectangle* position,
+      unsigned char width,
+      unsigned char height
+  );
 };
 
 } // namespace d2
--- SlashGaming-Diablo-II-API/src/cxx/game_struct/d2_equipment_layout/d2_equipment_layout_api.cc
+++ SlashGaming-Diablo-II-API/src/cxx/game_struct/d2_equipment_layout/d2_equipment_layout_api.cc
@@ -1,8 +1,8 @@
 /**
- * SlashGaming Diablo II Modding API
- * Copyright (C) 2018-2019  Mir Drualga
+ * SlashGaming Diablo II Modding API for C++
+ * Copyright (C) 2018-2020  Mir Drualga
  *
- * This file is part of SlashGaming Diablo II Modding API.
+ * This file is part of SlashGaming Diablo II Modding API for C++.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as published
@@ -45,65 +45,61 @@
 
 #include "../../../../include/cxx/game_struct/d2_equipment_layout/d2_equipment_layout_api.hpp"
 
-#include "d2_equipment_layout_impl.hpp"
 #include "../../../../include/cxx/game_struct/d2_positional_rectangle/d2_positional_rectangle_wrapper.hpp"
 #include "../../../../include/cxx/game_version.hpp"
 
 namespace d2 {
 namespace {
 
-using unique_ptr_1_00 = std::unique_ptr<EquipmentLayout_1_00>;
-
-using EquipmentLayoutVariant = std::variant<
-    unique_ptr_1_00
->;
-
-EquipmentLayoutVariant CreateVariant(
-    PositionalRectangle_View position,
-    std::int_least8_t width,
-    std::int_least8_t height
+template <typename EquipmentLayout_T>
+static std::unique_ptr<EquipmentLayout_T> CreatePtr(
+    const PositionalRectangle* position,
+    unsigned char width,
+    unsigned char height
 ) {
-  EquipmentLayout* equipment_layout = CreateEquipmentLayout(
-      position.Get(),
-      width,
-      height
-  );
+  using PositionalRectangle_T =
+      std::remove_pointer_t<decltype(EquipmentLayout_T::position)>;
 
-  return unique_ptr_1_00(
-      reinterpret_cast<EquipmentLayout_1_00*>(equipment_layout)
-  );
+  const PositionalRectangle_T* actual_positional_rectangle =
+      reinterpret_cast<const PositionalRectangle_T*>(position);
+
+  std::unique_ptr equipment_layout = std::make_unique<EquipmentLayout_T>();
+
+  equipment_layout->position = *actual_positional_rectangle;
+  equipment_layout->width = width;
+  equipment_layout->height = height;
+
+  return equipment_layout;
 }
 
 } // namespace
 
-EquipmentLayout_API::EquipmentLayout_API(
-    PositionalRectangle_View position,
-    std::int_least8_t width,
-    std::int_least8_t height
+EquipmentLayout_Api::EquipmentLayout_Api(
+    PositionalRectangle_View position, unsigned char width, unsigned char height
 ) : equipment_layout_(
-        CreateVariant(position, width, height)
+        CreateVariant(position.Get(), width, height)
     ) {
 }
 
-EquipmentLayout_API::EquipmentLayout_API(
-    const EquipmentLayout_API& other
-) : EquipmentLayout_API(
+EquipmentLayout_Api::EquipmentLayout_Api(
+    const EquipmentLayout_Api& other
+) : EquipmentLayout_Api(
         other.GetPosition(),
         other.GetWidth(),
         other.GetHeight()
     ) {
 }
 
-EquipmentLayout_API::EquipmentLayout_API(
-    EquipmentLayout_API&& other
+EquipmentLayout_Api::EquipmentLayout_Api(
+    EquipmentLayout_Api&& other
 ) noexcept = default;
 
-EquipmentLayout_API::~EquipmentLayout_API() = default;
+EquipmentLayout_Api::~EquipmentLayout_Api() = default;
 
-EquipmentLayout_API& EquipmentLayout_API::operator=(
-    const EquipmentLayout_API& other
+EquipmentLayout_Api& EquipmentLayout_Api::operator=(
+    const EquipmentLayout_Api& other
 ) {
-  *this = EquipmentLayout_API(
+  *this = EquipmentLayout_Api(
       other.GetPosition(),
       other.GetWidth(),
       other.GetHeight()
@@ -112,101 +108,83 @@ EquipmentLayout_API& EquipmentLayout_API::operator=(
   return *this;
 }
 
-EquipmentLayout_API& EquipmentLayout_API::operator=(
-    EquipmentLayout_API&& other
+EquipmentLayout_Api& EquipmentLayout_Api::operator=(
+    EquipmentLayout_Api&& other
 ) noexcept = default;
 
-EquipmentLayout_API::operator EquipmentLayout_View() const noexcept {
+EquipmentLayout_Api::operator EquipmentLayout_View() const noexcept {
   return EquipmentLayout_View(this->Get());
 }
 
-EquipmentLayout_API::operator EquipmentLayout_Wrapper() noexcept {
+EquipmentLayout_Api::operator EquipmentLayout_Wrapper() noexcept {
   return EquipmentLayout_Wrapper(this->Get());
 }
 
-EquipmentLayout* EquipmentLayout_API::Get() noexcept {
+EquipmentLayout* EquipmentLayout_Api::Get() noexcept {
   const auto* const_this = this;
 
   return const_cast<EquipmentLayout*>(const_this->Get());
 }
 
-const EquipmentLayout* EquipmentLayout_API::Get() const noexcept {
+const EquipmentLayout* EquipmentLayout_Api::Get() const noexcept {
   auto& actual_equipment_layout =
       std::get<unique_ptr_1_00>(this->equipment_layout_);
 
   return reinterpret_cast<const EquipmentLayout*>(actual_equipment_layout.get());
 }
 
-void EquipmentLayout_API::Copy(EquipmentLayout_View src) noexcept {
+void EquipmentLayout_Api::Assign(EquipmentLayout_View src) noexcept {
   EquipmentLayout_Wrapper wrapper(this->Get());
 
-  wrapper.Copy(src);
+  wrapper.Assign(src);
 }
 
-PositionalRectangle* EquipmentLayout_API::GetPosition() noexcept {
+PositionalRectangle* EquipmentLayout_Api::GetPosition() noexcept {
   EquipmentLayout_Wrapper wrapper(this->Get());
 
   return wrapper.GetPosition();
 }
 
-const PositionalRectangle* EquipmentLayout_API::GetPosition() const noexcept {
+const PositionalRectangle* EquipmentLayout_Api::GetPosition() const noexcept {
   EquipmentLayout_View view(this->Get());
 
   return view.GetPosition();
 }
 
-std::uint_least8_t EquipmentLayout_API::GetWidth() const noexcept {
+unsigned char EquipmentLayout_Api::GetWidth() const noexcept {
   EquipmentLayout_View view(this->Get());
 
   return view.GetWidth();
 }
 
-void EquipmentLayout_API::SetWidth(std::uint_least8_t value) noexcept {
+void EquipmentLayout_Api::SetWidth(unsigned char width) noexcept {
   EquipmentLayout_Wrapper wrapper(this->Get());
 
-  wrapper.SetWidth(value);
+  wrapper.SetWidth(width);
 }
 
-std::uint_least8_t EquipmentLayout_API::GetHeight() const noexcept {
+unsigned char EquipmentLayout_Api::GetHeight() const noexcept {
   EquipmentLayout_View view(this->Get());
 
   return view.GetHeight();
 }
 
-void EquipmentLayout_API::SetHeight(std::uint_least8_t value) noexcept {
+void EquipmentLayout_Api::SetHeight(unsigned char height) noexcept {
   EquipmentLayout_Wrapper wrapper(this->Get());
 
-  wrapper.SetHeight(value);
-}
-
-EquipmentLayout* CreateEquipmentLayout(
-    const PositionalRectangle* position,
-    std::int_least8_t width,
-    std::int_least8_t height
-) {
-  EquipmentLayout* equipment_layout;
-
-  equipment_layout = reinterpret_cast<EquipmentLayout*>(new EquipmentLayout_1_00());
-
-  // Set all the values of the struct.
-  EquipmentLayout_Wrapper wrapper(equipment_layout);
-
-  PositionalRectangle_Wrapper position_wrapper(wrapper.GetPosition());
-  position_wrapper.Copy(position);
-
-  wrapper.SetWidth(width);
   wrapper.SetHeight(height);
-
-  return equipment_layout;
 }
 
-void DestroyEquipmentLayout(EquipmentLayout* equipment_layout) {
-  d2::GameVersion running_game_version = d2::GetRunningGameVersionId();
-
-  auto* actual_equipment_layout =
-      reinterpret_cast<EquipmentLayout_1_00*>(equipment_layout);
-
-  delete actual_equipment_layout;
+EquipmentLayout_Api::ptr_variant EquipmentLayout_Api::CreateVariant(
+    const PositionalRectangle* position,
+    unsigned char width,
+    unsigned char height
+) {
+  return CreatePtr<EquipmentLayout_1_00>(
+      position,
+      width,
+      height
+  );
 }
 
 } // namespace d2
