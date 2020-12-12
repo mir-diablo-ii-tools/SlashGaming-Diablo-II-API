23f60bc3cd480af3a675e8010800dac166e2dbd2
IAmTrial <IAmRealTrial@gmail.com>
2020-12-12 00:48:26 -0800

    Fix incorrect buffer size allocation

--- SlashGaming-Diablo-II-API/src/cxx/game_struct/d2_unicode_char/d2_unicode_string_api.cc
+++ SlashGaming-Diablo-II-API/src/cxx/game_struct/d2_unicode_char/d2_unicode_string_api.cc
@@ -1085,7 +1085,7 @@ UnicodeString_Api UnicodeString_Api::FromUtf8String(
     std::u8string_view src
 ) {
   std::unique_ptr actual_temp_dest = 
-      std::make_unique<UnicodeChar_1_00[]>(src.length());
+      std::make_unique<UnicodeChar_1_00[]>(src.length() + 1);
   UnicodeChar* converted_temp_dest =
       reinterpret_cast<UnicodeChar*>(actual_temp_dest.get());
 
