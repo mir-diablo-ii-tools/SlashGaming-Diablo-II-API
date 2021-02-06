678fb3549bd86ce77efca5baaba1d515f82ad089
IAmTrial <IAmRealTrial@gmail.com>
2021-02-05 21:17:17 -0800

    Update copyright date on source files

--- SlashGaming-Diablo-II-API/src/cxx/game_variable/d2gfx/d2gfx_video_mode.cc
+++ SlashGaming-Diablo-II-API/src/cxx/game_variable/d2gfx/d2gfx_video_mode.cc
@@ -1,6 +1,6 @@
 /**
  * SlashGaming Diablo II Modding API for C++
- * Copyright (C) 2018-2020  Mir Drualga
+ * Copyright (C) 2018-2021  Mir Drualga
  *
  * This file is part of SlashGaming Diablo II Modding API for C++.
  *
